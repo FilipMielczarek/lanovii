5350d02a776d95024dd93676e5c106adf96d451d
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2022-02-15 13:41:22 +0100

    Basic theme configuration

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -1,67 +1,70 @@
 <?php
-/**
- * Jetpack Compatibility File
- *
- * @link https://jetpack.com/
- *
- * @package lanovii
- */
+  /**
+   * Jetpack Compatibility File
+   *
+   * @link https://jetpack.com/
+   *
+   * @package lanovii
+   */
 
-/**
- * Jetpack setup function.
- *
- * See: https://jetpack.com/support/infinite-scroll/
- * See: https://jetpack.com/support/responsive-videos/
- * See: https://jetpack.com/support/content-options/
- */
-function lanovii_jetpack_setup() {
-	// Add theme support for Infinite Scroll.
-	add_theme_support(
-		'infinite-scroll',
-		array(
-			'container' => 'main',
-			'render'    => 'lanovii_infinite_scroll_render',
-			'footer'    => 'page',
-		)
-	);
+  /**
+   * Jetpack setup function.
+   *
+   * See: https://jetpack.com/support/infinite-scroll/
+   * See: https://jetpack.com/support/responsive-videos/
+   * See: https://jetpack.com/support/content-options/
+   */
+  function lanovii_jetpack_setup()
+  {
+    // Add theme support for Infinite Scroll.
+    add_theme_support(
+      'infinite-scroll',
+      array(
+        'container' => 'main',
+        'render' => 'lanovii_infinite_scroll_render',
+        'footer' => 'page',
+      )
+    );
 
-	// Add theme support for Responsive Videos.
-	add_theme_support( 'jetpack-responsive-videos' );
+    // Add theme support for Responsive Videos.
+    add_theme_support('jetpack-responsive-videos');
 
-	// Add theme support for Content Options.
-	add_theme_support(
-		'jetpack-content-options',
-		array(
-			'post-details' => array(
-				'stylesheet' => 'lanovii-style',
-				'date'       => '.posted-on',
-				'categories' => '.cat-links',
-				'tags'       => '.tags-links',
-				'author'     => '.byline',
-				'comment'    => '.comments-link',
-			),
-			'featured-images' => array(
-				'archive' => true,
-				'post'    => true,
-				'page'    => true,
-			),
-		)
-	);
-}
-add_action( 'after_setup_theme', 'lanovii_jetpack_setup' );
+    // Add theme support for Content Options.
+    add_theme_support(
+      'jetpack-content-options',
+      array(
+        'post-details' => array(
+          'stylesheet' => 'lanovii-style',
+          'date' => '.posted-on',
+          'categories' => '.cat-links',
+          'tags' => '.tags-links',
+          'author' => '.byline',
+          'comment' => '.comments-link',
+        ),
+        'featured-images' => array(
+          'archive' => true,
+          'post' => true,
+          'page' => true,
+        ),
+      )
+    );
+  }
 
-if ( ! function_exists( 'lanovii_infinite_scroll_render' ) ) :
-	/**
-	 * Custom render function for Infinite Scroll.
-	 */
-	function lanovii_infinite_scroll_render() {
-		while ( have_posts() ) {
-			the_post();
-			if ( is_search() ) :
-				get_template_part( 'template-parts/content', 'search' );
-			else :
-				get_template_part( 'template-parts/content', get_post_type() );
-			endif;
-		}
-	}
-endif;
+  add_action('after_setup_theme', 'lanovii_jetpack_setup');
+
+  if (!function_exists('lanovii_infinite_scroll_render')) :
+    /**
+     * Custom render function for Infinite Scroll.
+     */
+    function lanovii_infinite_scroll_render()
+    {
+      while (have_posts()) {
+        the_post();
+        if (is_search()) :
+          get_template_part('template-parts/content', 'search');
+        else :
+          get_template_part('template-parts/content', get_post_type());
+        endif;
+      }
+    }
+  endif;
---
+++ templates/homepage.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Homepage */
+  get_header(); ?>
+<div class="container">
+  <div class="homepage">
+    <p>This is a test paragraph</p>
+  </div>
+</div>
+<?php get_footer(); ?>
