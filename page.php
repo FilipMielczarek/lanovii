867f8a9d3ce8c3c16384c974d3c736e0ca30f780
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2022-03-01 19:35:02 +0100

    Coherent styles, dropdown menus

--- page.php
+++ page.php
@@ -15,7 +15,7 @@
     get_header();
 ?>
 
-    <main id="primary" class="site-main">
+    <main id="primary" class="site-main standard-margins">
         <div class="container">
             <?php
                 while (have_posts()) :
