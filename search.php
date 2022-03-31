7f6959a0b8b7688a9cf387a6d7528d3c9940b7a4
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2022-03-31 19:31:23 +0200

    Search results and woocommerce pagination style

--- search.php
+++ search.php
@@ -1,52 +1,27 @@
-<?php
-    /**
-     * The template for displaying search results pages
-     *
-     * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
-     *
-     * @package lanovii
-     */
-
-    get_header();
-?>
-
+<?php get_header(); ?>
     <div id="primary" class="site-main">
-
-        <?php if (have_posts()) : ?>
-
-            <header class="page-header">
+        <div class="container">
+            <?php if (have_posts()) : ?>
+            <header class="page-header page-header__search">
                 <h1 class="page-title">
                     <?php
-                        /* translators: %s: search query. */
                         printf(esc_html__('Search Results for: %s', 'lanovii'), '<span>' . get_search_query() . '</span>');
                     ?>
                 </h1>
-            </header><!-- .page-header -->
+            </header>
 
-            <?php
-            /* Start the Loop */
-            while (have_posts()) :
-                the_post();
+            <section class="product-search">
+                <?php while (have_posts()) :
+                    the_post();
+                    get_template_part('template-parts/content', 'search');
 
-                /**
-                 * Run the loop for the search to output the results.
-                 * If you want to overload this in a child theme then include a file
-                 * called content-search.php and that will be used instead.
-                 */
-                get_template_part('template-parts/content', 'search');
-
-            endwhile;
-
-            the_posts_navigation();
-
-        else :
-
-            get_template_part('template-parts/content', 'none');
-
-        endif;
-        ?>
-
-    </div><!-- #main -->
-
-<?php
-    get_footer();
+                endwhile;
+                    the_posts_navigation();
+                    else :
+                        get_template_part('template-parts/content', 'none');
+                    endif;
+                ?>
+            </section>
+        </div>
+    </div>
+<?php get_footer();
