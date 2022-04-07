d1262d1060a2a2301247e0f07034c5d48c991408
Filip Mielczarek <mielczarek.filip.1998@gmail.com>
2022-04-07 16:11:56 +0200

    Display tabs as blocks

--- functions.php
+++ functions.php
@@ -136,6 +136,13 @@
 
     add_action('after_setup_theme', 'lanovii_add_woocommerce_support');
 
+    function disable_woo_commerce_sidebar()
+    {
+        remove_action('woocommerce_sidebar', 'woocommerce_get_sidebar', 10);
+    }
+
+    add_action('init', 'disable_woo_commerce_sidebar');
+
     require get_template_directory() . '/inc/custom-header.php';
     require get_template_directory() . '/inc/template-tags.php';
     require get_template_directory() . '/inc/template-functions.php';
